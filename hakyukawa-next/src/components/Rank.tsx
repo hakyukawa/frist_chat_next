6ed2cb30337c529efd4a299bd3193a72f8ace4ff
sojiro takagi <soujirou.takagi@gmail.com>
2025-03-04 14:02:30 +0900

    feat: add Rank component with basic structure and props interface

---
+++ hakyukawa-next/src/components/Rank.tsx
@@ -0,0 +1,13 @@
+interface RankProps {
+    rank: number;
+    points: number;
+}
+
+export default function Rank() {
+    return (
+        <div>
+            <h1>Rank.</h1>
+            <p>Welcome to the Rank page!</p>
+        </div>
+    );
+}
