d4ecc36aeb9ea13c83cf8edac16548b97af18650
rikuto-mania <rikutoland55@gmail.com>
2025-03-04 18:06:19 +0900

    [add]ログイン画面スタイル適用

--- hakyukawa-next/src/app/login/page.jsx
+++ hakyukawa-next/src/app/login/page.jsx
@@ -1,8 +1,9 @@
 import React  from "react";
+import styles from "@/styles/componentStyles/login/Login.css";
 
 function InputField (props) {
     return(
-        <div>
+        <div className="form">
             <label>{props.label}</label>
             <input type={props.type} name= {props.name}  className="textbox" placeholder={props.placeholder}></input>
         </div>
@@ -12,18 +13,19 @@ function InputField (props) {
 
 function Login () {
     return(
-        <div>
-            <h2 className="text-center">ログイン</h2>
+        <div className= "loginwrap">
+            <h2 id="title">ログイン</h2>
             <form action="POST">
                 <div id="loginform">
                     <InputField label = "ユーザーID" type = "text" name = "userId" placeholder = "ユーザIDを入力"/>
                     <InputField label = "パスワード" type = "password" name = "password" placeholder = "" />
-                    <small>パスワードをお忘れの方</small>
+                    <small id="forgetpassword">パスワードをお忘れの方</small>
                 </div>
-                <input type="submit" value="ログイン"></input>
+                <input type="submit" value="ログイン" id="login"></input>
             </form>
-            <small>アカウントをお持ちでない方は登録</small>
+            <small id="signUp">アカウントをお持ちでない方は登録</small>
         </div>
     );
 }
 
+export default Login;
