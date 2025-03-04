ef9943dd462b03b503959c669b1a5c78c58a7e25
sojiro takagi <soujirou.takagi@gmail.com>
2025-03-04 15:13:28 +0900

    feat: enhance Rank component with SCSS styling and class name management

--- hakyukawa-next/src/components/common/Rank.tsx
+++ hakyukawa-next/src/components/common/Rank.tsx
@@ -1,3 +1,5 @@
+import styles from "@/styles/componentStyles/common/Rank.module.scss";
+import clsx from "clsx";
 interface RankProps {
     rank: number;
     points: number;
@@ -5,9 +7,14 @@ interface RankProps {
 
 export default function Rank(props: RankProps) {
     return (
-        <div>
-            <h1>Rank.{props.rank}</h1>
-            <div></div>
+        <div className={styles.rankWrap}>
+            <h2>
+                Rank.<span>{props.rank}</span>
+            </h2>
+            <div className={styles.rankBarWrap}>
+                <div className={clsx(styles.rankGrayBar, styles.rankBars)}></div>
+                <div className={clsx(styles.rankBlueBar, styles.rankBars)}></div>
+            </div>
             <p>{props.points}/5000</p>
         </div>
     );
