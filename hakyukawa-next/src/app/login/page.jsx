4c72574b737c6a2ef2342b02ebeed564800b90eb
rikuto-mania <rikutoland55@gmail.com>
2025-03-05 12:09:30 +0900

    [fix]クラス名修正

--- hakyukawa-next/src/app/login/page.jsx
+++ hakyukawa-next/src/app/login/page.jsx
@@ -3,7 +3,7 @@ import styles from "@/styles/componentStyles/login/Login.css";
 
 function InputField (props) {
     return(
-        <div className="form">
+        <div className="input">
             <label>{props.label}</label>
             <input type={props.type} name= {props.name}  className="textbox" placeholder={props.placeholder}></input>
         </div>
@@ -15,15 +15,15 @@ function Login () {
     return(
         <div className= "loginwrap">
             <h2 id="title">ログイン</h2>
-            <form action="POST">
-                <div id="loginform">
+            <form method="POST" >
+                <div id="loginforms">
                     <InputField label = "ユーザーID" type = "text" name = "userId" placeholder = "ユーザIDを入力"/>
                     <InputField label = "パスワード" type = "password" name = "password" placeholder = "" />
-                    <small id="forgetpassword">パスワードをお忘れの方</small>
+                    <small className="forgetpassword"><a href="">パスワードをお忘れの方</a></small>
                 </div>
                 <input type="submit" value="ログイン" id="login"></input>
             </form>
-            <small id="signUp">アカウントをお持ちでない方は登録</small>
+            <small id="signUp">アカウントをお持ちでない方は<span className="links"><a href="">登録</a></span></small>
         </div>
     );
 }
