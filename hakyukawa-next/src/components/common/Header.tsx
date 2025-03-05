7b2a9dcc73ed602e00f882e87aba65a629a1e3cf
sojiro takagi <soujirou.takagi@gmail.com>
2025-03-05 13:44:02 +0900

    [update]Headerコンポーネントをスタイルのみ追加

--- hakyukawa-next/src/components/common/Header.tsx
+++ hakyukawa-next/src/components/common/Header.tsx
@@ -1,36 +1,36 @@
 import { GoPersonAdd } from "react-icons/go";
 import { GoBell } from "react-icons/go";
-import { TbSettings } from "react-icons/tb";
+import { IoSettingsOutline } from "react-icons/io5";
 import { AiOutlineUsergroupAdd } from "react-icons/ai";
 import { IoIosArrowBack } from "react-icons/io";
-import { IoSearch } from "react-icons/io5";
+import { IoIosSearch } from "react-icons/io";
 import { BsFire } from "react-icons/bs";
 
 interface HeaderProps {
-    setting?: boolean;
+    backPage?: boolean;
+    backPageText?: string;
+    core?: boolean;
     addFriend?: boolean;
     addGroup?: boolean;
     notice?: boolean;
     search?: boolean;
-    core?: boolean;
-    backPage?: boolean;
-    backPageText?: string;
+    setting?: boolean;
 }
 
 export default function Header(props: HeaderProps) {
     return (
-        <header className="flex justify-between items-center p-4">
-            <div className="flex items-center">
-                {props.backPage && <IoIosArrowBack />}
-                {props.backPageText && <p>{props.backPageText}</p>}
+        <header className="flex justify-between items-center p-4 text-[20px]">
+            <div className="flex items-center font-semibold">
+                {props.backPage && <IoIosArrowBack size="25px" style={{ marginRight: "5px" }} />}
+                {props.backPageText && <p style={{ marginRight: "5px" }}>{props.backPageText}</p>}
                 {props.core && <BsFire />}
             </div>
-            <div>
+            <div className="flex text-[26px]">
                 {props.addFriend && <GoPersonAdd />}
-                {props.addGroup && <AiOutlineUsergroupAdd />}
-                {props.notice && <GoBell />}
-                {props.search && <IoSearch />}
-                {props.setting && <TbSettings />}
+                {props.addGroup && <AiOutlineUsergroupAdd style={{ marginLeft: "5px" }} />}
+                {props.notice && <GoBell style={{ marginLeft: "5px" }} />}
+                {props.search && <IoIosSearch style={{ marginLeft: "5px" }} />}
+                {props.setting && <IoSettingsOutline style={{ marginLeft: "5px" }} />}
             </div>
         </header>
     );
