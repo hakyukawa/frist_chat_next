2218b151bdfa56078354b63fb833c1d7db6e73aa
sojiro takagi <soujirou.takagi@gmail.com>
2025-03-05 17:41:52 +0900

    [feature]switchボタンコンポーネントを追加

---
+++ hakyukawa-next/src/components/common/NoticeSwitchButton.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import Switch, { SwitchProps } from "@mui/material/Switch";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import { styled } from "@mui/material/styles";
+import FormGroup from "@mui/material/FormGroup";
+
+const IOSSwitch = styled((props: SwitchProps) => (
+    <Switch focusVisibleClassName=".Mui-focusVisible" disableRipple {...props} />
+))(({ theme }) => ({
+    width: 42,
+    height: 26,
+    padding: 0,
+    "& .MuiSwitch-switchBase": {
+        padding: 0,
+        margin: 2,
+        transitionDuration: "300ms",
+        "&.Mui-checked": {
+            transform: "translateX(16px)",
+            color: "#2E2F34",
+            "& + .MuiSwitch-track": {
+                backgroundColor: "#EC891F",
+                opacity: 1,
+                border: 0,
+            },
+        },
+        "&.Mui-disabled + .MuiSwitch-track": {
+            opacity: 0.5,
+        },
+    },
+    "& .MuiSwitch-thumb": {
+        boxSizing: "border-box",
+        width: 22,
+        height: 22,
+        backgroundColor: "#2E2F34",
+    },
+    "& .MuiSwitch-track": {
+        borderRadius: 26 / 2,
+        backgroundColor: "#AFAFAF",
+        opacity: 1,
+        transition: theme.transitions.create(["background-color"], {
+            duration: 500,
+        }),
+    },
+}));
+
+interface NoticeSwitchButtonProps {
+    label: string;
+}
+
+export default function NoticeSwitchButton(props: NoticeSwitchButtonProps) {
+    return (
+        <div className="flex items-center justify-between">
+            <p className="font-semibold text-[1.4rem]">{props.label}</p>
+            <FormGroup>
+                <FormControlLabel
+                    sx={{ mr: 0 }}
+                    control={<IOSSwitch sx={{ mx: 0, my: 1 }} defaultChecked />}
+                    label=""
+                />
+            </FormGroup>
+        </div>
+    );
+}
