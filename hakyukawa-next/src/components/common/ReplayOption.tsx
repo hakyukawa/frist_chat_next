0fbebb9d80f8b3159a30613e41bcfde222401fa4
sojiro takagi <soujirou.takagi@gmail.com>
2025-03-07 14:54:47 +0900

    返信設定コンポーネントの作成

--- hakyukawa-next/src/components/common/ReplayOption.tsx
+++ hakyukawa-next/src/components/common/ReplayOption.tsx
@@ -1,15 +1,31 @@
-import Select from "@mui/material/Select";
+"use client";
+import Select, { SelectChangeEvent } from "@mui/material/Select"; // SelectChangeEventをインポート
 import MenuItem from "@mui/material/MenuItem";
+import ReplayTimeHeadline from "./ReplayTimeHeadline";
+import { useState } from "react";
 
 export default function ReplayOption() {
+    const [selectedTime, setSelectedTime] = useState<string>("");
+
+    // handleChangeの引数をSelectChangeEvent<string>に修正
+    const handleChange = (event: SelectChangeEvent<string>) => {
+        setSelectedTime(event.target.value);
+    };
+
     return (
         <div>
-            <h2 className="text-subText font-semibold text-[14px] ">返信設定</h2>
-            <div className="bg-[#FDBB71] h-[90px] rounded-[10px]">
-                <div className="p-[14px] ">
+            <h2 className="text-subText font-semibold text-[14px]">返信設定</h2>
+            <div className="bg-[#FDBB71] h-[90px] rounded-[10px] py-3 my-3">
+                <ReplayTimeHeadline label="目標返信時間" />
+                <div className="px-[14px] py-3">
                     <Select
+                        value={selectedTime} // stateから値を取得
+                        onChange={handleChange} // 値が変更されたときに更新
                         sx={{
                             width: "100%",
+                            height: "35px",
+                            borderRadius: "12px",
+                            fontSize: "1.3rem",
                             backgroundColor: "#2E2F34", // 背景色を#2E2F34に設定
                             color: "#ffffff", // 文字色を白に
                             "& .MuiSelect-icon": {
