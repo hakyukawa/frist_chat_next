f3b8b1bc7e15cde06d8b868bcd239819a279287f
sojiro takagi <soujirou.takagi@gmail.com>
2025-03-07 15:01:12 +0900

    [update]スタイルの修正

--- hakyukawa-next/src/components/common/ReplayTimeHeadline.tsx
+++ hakyukawa-next/src/components/common/ReplayTimeHeadline.tsx
@@ -7,7 +7,7 @@ interface ReplayTimeHeadlineProps {
 export default function ReplayTimeHeadline(props: ReplayTimeHeadlineProps) {
     return (
         <div
-            className="bg-gradient-to-r text-background from-[#FE950F] to-[#F8644A] h-[27px] w-[162px] text-[14px] font-semibold flex items-center justify-evenly pr-[40px] pl-[14px]"
+            className="bg-gradient-to-r text-background from-[#F8644A] to-[#FE950F] h-[27px] w-[162px] text-[14px] font-semibold flex items-center justify-evenly pr-[40px] pl-[14px]"
             style={{ clipPath: "polygon(0 0, 85% 0, 100% 100%, 0% 100%)" }}
         >
             <BsFire />
