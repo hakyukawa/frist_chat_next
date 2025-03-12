4ce70656b3c75ffb8a292af72c1cde91ad7bde9a
rikuto-mania <rikutoland55@gmail.com>
2025-03-12 11:31:30 +0900

    [add]アカウント削除画面作成

--- hakyukawa-next/src/app/deleteUser/page.tsx
+++ hakyukawa-next/src/app/deleteUser/page.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Header from "@/components/common/Header";
 import User from "@/components/common/option/User";
-import SubmitButton from "@/components/common/SubmitButton";
+import DeleteButton from "@/components/common/option/DeleteButton";
 
 function Deleteuser () {
     const nowUser = "hakyukawa"
     const nowID ="hakyukawa-test"
     return(
         <div>   
-            <Header backPage backPageLink="/" backPageText="アカウントを削除する" />
+            <Header backPage backPageLink="/configMenu" backPageText="アカウントを削除する" />
             <div className="p-4">
                 <User 
                     userName={nowUser}
@@ -16,7 +16,13 @@ function Deleteuser () {
                 />
                 <p className="text-[15px] font-light">アカウントが削除されます</p>
                 <p className="text-[12px] font-nomal">「アカウント削除」をタップするとデータが全て削除され、ログインができなくなります。<br />
-                <span className="text-red-500">削除されたアカウントは復旧できません。</span></p>
+                <span className="text-red-500">削除されたアカウントは復旧できません。</span></p>   
+            </div>
+            <div className="px-[86px] py-[15px]">
+                <DeleteButton 
+                    buttonValue="アカウントを削除"
+                    className="p-[10px] border border-red-500 rounded-[40px] w-full text-red-500 font-semibold"
+                />
             </div>
             
         </div>
