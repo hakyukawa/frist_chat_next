e03d3d55294e2d3158ba9958e977f877b333c66b
rikuto-mania <rikutoland55@gmail.com>
2025-03-12 14:37:38 +0900

    [fix]ディレクトリ修正

---
+++ hakyukawa-next/src/app/config/passwordConfig/page.jsx
@@ -0,0 +1,80 @@
+"use client"; 
+import React, {useState} from "react";
+import Header from "@/components/common/Header";
+import PasswordForm from "@/components/common/option/passwordForm";
+import SubmitButton from "@/components/common/SubmitButton";
+
+function PasswordConfig (){
+     //入力・エラー状態管理
+    const [formData,setFormData] = useState({
+        password: "",
+        password_confirmation: "",
+    })
+    const [errors,setErrors] = useState({});
+
+    //入力内容の変更を反映
+    const handleChange = (e) => {
+        const {name,value} = e.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
+
+    //バリデーション
+    const validate = () => {
+        const newErrors = {};
+
+        if (!formData.password) newErrors.password = "記入してください";
+        else if (formData.password.length < 8) {
+            newErrors.password = "パスワードは8文字以上で入力してください";
+            newErrors.password_confirmation = "パスワードは8文字以上で入力してください";
+        }
+
+        if (!formData.password_confirmation) newErrors.password_confirmation = "記入してください";
+        else if (formData.password !== formData.password_confirmation) {
+            newErrors.password_confirmation = "パスワードが一致しません";
+            newErrors.password = "パスワードが一致しません";
+        }
+
+        setErrors(newErrors);
+        return Object.keys(newErrors).length === 0;
+    };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // バリデーションを実行
+        if (validate()) {
+            console.log("フォーム送信");
+            alert("登録が完了しました");
+        }else{
+            alert("エラー");
+        }
+    };
+
+    return(
+        <div>
+            <Header backPage backPageLink ="/config" backPageText="パスワードを変更"  />
+            <form onSubmit={handleSubmit} action="" method="POST" className="p-[16px]">
+                <PasswordForm
+                    label="パスワード"
+                    inputName="password"
+                    formData ={formData.password}
+                    error={errors.password}
+                    onChange = {handleChange}
+                />
+                <PasswordForm
+                    label="パスワード(確認用)"
+                    inputName="password_confirmation"
+                    formData ={formData.password_confirmation}
+                    error={errors.password_confirmation}
+                    onChange = {handleChange}
+                />
+                <SubmitButton 
+                    buttonValue="変更"
+                />
+            </form>
+        </div>
+    );
+}
+
+export default PasswordConfig;
---
+++ hakyukawa-next/src/app/config/useridconfig/page.jsx
@@ -0,0 +1,81 @@
+"use client"; 
+import React, {useState} from "react";
+import Header from "@/components/common/Header";
+import Status from "@/components/common/option/Status";
+import SubmitButton from "@/components/common/SubmitButton";
+import InputForm from "@/components/common/option/InputForm";
+
+function UserIdConfig () {
+    //テスト用ユーザーID
+    const nowuserid = "hakyukawa-test";
+    const demouserid = ["user01","user02","user03"];
+    
+    //入力・エラー状態管理
+    const [formData,setFormData] = useState({
+        newUserId: "",
+    });
+    const [errors,setErrors] = useState({});
+
+    //入力内容の変更を反映
+    const handleChange = (e) => {
+        const {name,value} = e.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
+
+    //バリデーション
+    const validate = () => {
+        const newErrors = {};
+
+        if(!formData.newUserId){
+            newErrors.newUserId = "記入してください";
+        }else if(nowuserid.includes(formData.newUserId)){
+            newErrors.newUserId ="現在と同じユーザーIDは使用できません!";
+        }else if (demouserid.includes(formData.newUserId)){
+            newErrors.newUserId = "このユーザーIDは既に使用されています"
+        }else if(!/^[0-9A-Za-z._-]+$/.test(formData.newUserId)){
+            newErrors.newUserId ="半角英数字、アンダーバー（_）、ピリオド（.）、ハイフン（-）のみ使用できます"
+        }
+
+        setErrors(newErrors);
+        return Object.keys(newErrors).length === 0;
+    };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // バリデーションを実行
+        if (validate()) {
+            console.log("フォーム送信");
+            alert("登録が完了しました");
+        }else{
+            console.log("変更に失敗しました");
+        }
+    };
+    
+    return(
+        <div>  
+            <Header backPage backPageLink ="/config" backPageText="ユーザーIDを変更" />
+            <form onSubmit={handleSubmit} action="" method="POST" className="p-[16px]" >
+                <Status
+                    nowState={nowuserid}
+                />
+                <InputForm 
+                    label={"新しいユーザーID"}
+                    inputName ="newUserId"
+                    formData =  {formData.newUserId}
+                    error = {errors.newUserId}
+                    onChange = {handleChange}
+                />
+                <SubmitButton 
+                    buttonValue="送信"
+                />  
+            </form>
+        </div>
+    );
+}
+
+export default UserIdConfig;
+
+
+
