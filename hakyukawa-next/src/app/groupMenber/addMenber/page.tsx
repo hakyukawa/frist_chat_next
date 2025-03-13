597d9265ff1a40249b812816ce73c2917115696d
rikuto-mania <rikutoland55@gmail.com>
2025-03-13 16:22:24 +0900

    [update]追加ボタンを押すと非表示になる機能を追加

--- hakyukawa-next/src/app/groupMenber/addMenber/page.tsx
+++ hakyukawa-next/src/app/groupMenber/addMenber/page.tsx
@@ -8,23 +8,23 @@ import { FiPlusCircle } from "react-icons/fi";
 function Addmenber () {
     const [searchQuery, setSearchQuery] = useState("");
 
-    const UsersArray = [
-        {id:1, userName: "ユーザー1", userId:"user01"},
-        {id:2, userName: "はきゅかわさん", userId:"hakyukawasan"},
-        {id:3, userName: "餃子", userId:"user03"},
-        {id:4, userName: "ユーザー11", userId:"user11"},
-        {id:5, userName: "ユーザー45", userId:"user45"},
-        {id:6, userName: "ユーザー66", userId:"user66"},
-        {id:7, userName: "ユーザー72", userId:"user72"},
-        {id:8, userName: "ユーザー79", userId:"user79"},
-        {id:9, userName: "ユーザー94", userId:"user94"},
-    ];
+    //テスト用ユーザー
+    const [users,setUsers] = useState([
+            {id:1, userName: "ユーザー1", userId:"user01"},
+            {id:2, userName: "はきゅかわさん", userId:"hakyukawasan"},
+            {id:3, userName: "餃子", userId:"user03"},
+            {id:4, userName: "ユーザー2", userId:"user11"},
+            {id:5, userName: "ユーザー3", userId:"user45"},
+            {id:6, userName: "ユーザー4", userId:"user66"},
+            {id:7, userName: "ユーザー5", userId:"user72"},
+        ]); 
 
-    const filterSearch = UsersArray.filter((user) =>{
+    const filterSearch = users.filter((user) =>{
         return user.userId.toLowerCase().includes(searchQuery.toLowerCase()) || user.userName.toLowerCase().includes(searchQuery.toLowerCase())
     });
 
-    const handleClick = () =>{
+    const addUser = (userId:string) =>{
+        setUsers((preUsers) => preUsers.filter(user => user.userId !== userId));
         console.log("メンバーを追加しました！");
     }
 
@@ -39,7 +39,7 @@ function Addmenber () {
                             userName={users.userName}
                             userId={users.userId}
                         />
-                        <button onClick={handleClick} type="button" className="flex justify-center items-center ">
+                        <button onClick={() => addUser(users.userId)} type="button" className="flex justify-center items-center ">
                             <FiPlusCircle size={26} className="text-main"/>
                         </button>
                     </div>
