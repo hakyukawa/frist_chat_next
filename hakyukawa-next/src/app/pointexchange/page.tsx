8777868e317502d6c1578ef1c0cc4067d300b954
sojiro takagi <soujirou.takagi@gmail.com>
2025-03-14 22:24:56 +0900

    [update]icon追加

--- hakyukawa-next/src/app/pointexchange/page.tsx
+++ hakyukawa-next/src/app/pointexchange/page.tsx
@@ -1,23 +1,29 @@
+"use client";
+
 import React from "react";
 import Header from "@/components/common/Header";
+// import { useProfile } from "@/hooks/useProfile";
+// import { useItems } from "@/hooks/useItems";
 
-export default function Pointexchange () {
+export default function PointExchange() {
+    const nowPoint = 1300;
+    // const { data: user } = useProfile();
+    // const { data: item } = useItems();
 
-    const nowPoint = 13300;
-
-    return(
+    return (
         <div>
             <Header backPage backPageLink="/home" backPageText="ポイント引き換え" />
             <div className="p-4">
                 <div className="border border-main rounded-[10px]">
-                    <div className="text-center">
-                        <p className="text-[13px]">所持ポイント</p>
-                        <p className="text-[27px] font-extrabold">{nowPoint}<span className="text-[20px]">pt</span></p>
-                    </div>  
+                    <div className="text-center py-4">
+                        <p className="text-[1.3rem] font-semibold">所持ポイント</p>
+                        <p className="text-[2.7rem] text-main font-extrabold">
+                            {nowPoint}
+                            <span className="text-[20px]">pt</span>
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
-    )
-
-    
+    );
 }
